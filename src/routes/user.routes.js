90c1c8a0a0f2ac8894b9ec0f8685bf855997900d
umakant3525 <umakant3525@gmail.com>
2024-04-01 20:00:21 +0530

    debugging user channel profile

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -44,7 +44,7 @@ router.route("/update-account").patch(verifyJWT, updateAccountDetails)
 router.route("/update-avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
 router.route("/update-cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
 
-router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/getchannel/:username").get(verifyJWT, getUserChannelProfile)
 router.route("/history").get(verifyJWT, getWatchHistory)
 
 export default router
