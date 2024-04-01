0243977e8e63b2a6bd03d5b198af13b9a7cfa05e
umakant3525 <umakant3525@gmail.com>
2024-04-01 18:47:56 +0530

    debug with change password

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -232,21 +232,22 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
 })
 
 const changeCurrentPassword = asyncHandler(async(req, res) => {
-    const {oldPassword, newPassword} = req.body
+     const {oldPassword, newPassword} = req.body
 
-    const user = await User.findById(req.user?._id)
-    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
+     const user = await User.findById(req.user?._id)
+
+     const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
 
     if (!isPasswordCorrect) {
         throw new ApiError(400, "Invalid old password")
     }
+    
+     user.password = newPassword
+     await user.save({validateBeforeSave: false})
 
-    user.password = newPassword
-    await user.save({validateBeforeSave: false})
-
-    return res
-    .status(200)
-    .json(new ApiResponse(200, {}, "Password changed successfully"))
+     return res
+     .status(200)
+     .json(new ApiResponse(200, {}, "Password changed successfully"))
 })
 
 
