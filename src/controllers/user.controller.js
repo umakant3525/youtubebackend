640207888951312415298c136feeb062dd187c8b
umakant3525 <umakant3525@gmail.com>
2024-04-01 18:30:42 +0530

    routers checking till register login logout refresh works

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -234,8 +234,6 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
 const changeCurrentPassword = asyncHandler(async(req, res) => {
     const {oldPassword, newPassword} = req.body
 
-    
-
     const user = await User.findById(req.user?._id)
     const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
 
