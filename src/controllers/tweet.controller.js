ace039779b0fe98c96932b2371b237a32b7c0954
umakant3525 <umakant3525@gmail.com>
2024-04-02 21:49:36 +0530

    tweet created

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -5,9 +5,24 @@ import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
+
 const createTweet = asyncHandler(async (req, res) => {
-    //TODO: create tweet
-})
+    const { content } = req.body;
+ //TODO: create tweet
+
+    if (!content) {
+        throw new ApiError(400, "Something comment content is required is required field are required");
+    }
+
+    const tweetObject = await Tweet.create({
+      content,
+      owner: req.user._id 
+    });
+
+    return res.status(201).json(
+        new ApiResponse(200, tweetObject, "Tweet added successfully")
+    );
+});
 
 const getUserTweets = asyncHandler(async (req, res) => {
     // TODO: get user tweets
