18411c3c329255a27a83c1af503a3012d5fb5527
umakant3525 <umakant3525@gmail.com>
2024-04-03 13:13:17 +0530

    update comment with correct owner

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -12,6 +12,7 @@ const getVideoComments = asyncHandler(async (req, res) => {
 })
 
 const addComment = asyncHandler(async (req, res) => {
+     // TODO: add a comment
     const { videoId } = req.params;
     const { content } = req.body;
 
@@ -39,11 +40,32 @@ const addComment = asyncHandler(async (req, res) => {
     );
 });
 
-
-
 const updateComment = asyncHandler(async (req, res) => {
     // TODO: update a comment
-})
+    const { commentId } = req.params;
+    const { content } = req.body;
+
+    if (!content || !commentId) {
+        throw new ApiError(400, "Content and comment ID are required");
+    }
+
+    try {
+        const updatedComment = await Comment.findOneAndUpdate(
+            { _id: commentId, owner: req.user._id }, 
+            { content: content }, 
+            { new: true } 
+        );
+
+        if (!updatedComment) {
+            throw new ApiError(404, "Comment not found or you don't have permission to update");
+        }
+        return res.status(200).json(
+            new ApiResponse(200, updatedComment, "Comment updated successfully")
+        );
+    } catch (error) {
+        throw new ApiError(500, "Error updating comment");
+    }
+});
 
 const deleteComment = asyncHandler(async (req, res) => {
     // TODO: delete a comment
