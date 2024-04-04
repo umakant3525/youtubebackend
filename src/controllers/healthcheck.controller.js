471ab4a4f16fe1b6e4c1ef086f85486f2422be58
umakant3525 <umakant3525@gmail.com>
2024-04-04 12:09:16 +0530

    health

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -4,11 +4,17 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 
 const healthcheck = asyncHandler(async (req, res) => {
-    //TODO: build a healthcheck response that simply returns the OK status as json with a message
-    res.status(200).json({ message : "My heath is ok "})
-})
+    try {
+        return res.status(200).json(new ApiResponse(200, null, "My health is ok"));
+    } catch (error) {
+        throw new ApiError(500, "Internal Server Error");
+    }
+});
 
 export {
     healthcheck
-    }
+};
+
+
+
     
